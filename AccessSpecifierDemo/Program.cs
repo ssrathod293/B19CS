526c2383eb6544d45781a5778d39bdda70161d9b
Shrikant Rathod <ssrathod293@gmail.com>
2023-03-26 11:47:52 +0530

    acess modifier code added

--- AccessSpecifierDemo/Program.cs
+++ AccessSpecifierDemo/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//access specifiers
+/*
+ * controls accessiblity of type or type members
+ * -type like class,struct,interface,delegate etc
+ * -type member like feild, constructor,method,property ,indexer
+ * 1.private-
+ * 2.protected
+ * 3.public
+ * 4.internal
+ * 5.protected internal
+ * with type(inside namespace)--public or internal--defualt--internal
+ * with type member(inside type)--all acess specifier ...defualt--private
+ * */
 namespace AccessSpecifierDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Assembly1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assembly2;
+
+namespace Assembly1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class X:A
+    {
+        void printX()
+        {
+            //A a = new A();
+            //a.printA();
+            printA();
+            Console.WriteLine("X");
+        }
+    }
+    class Y
+    {
+        public Y()
+        {
+            A a = new A();
+            a.printA();
+        }
+    }
+}
